33d6e5197dcd116e9f9962df88d8aa41164db319
eirihoyh <eirik.hoy@gmail.com>
2026-04-05 13:46:44 +0200

    added what-if explanations

--- LBBNN/__init__.py
+++ LBBNN/__init__.py
@@ -1,5 +1,3 @@
-# from .lrt.layers import BayesianLinear, get_default_device
-# from .lrt.network import BayesianNetwork, InputSkipLRTNetwork
 from .data import create_data_unif, create_bsr_data, get_data
 from .inspection import (
     nr_hidden_layers, weight_matrices, weight_matrices_numpy, weight_matrices_std,
@@ -11,7 +9,8 @@ from .inspection import (
 )
 from .explain import (
     relu_activation, local_explain_relu,
-    local_explain_relu_magnitude, local_explain_piecewise_linear_act,
+    local_explain_relu_magnitude, local_explain_piecewise_linear_act, 
+    what_if_explanations
 )
 from .training import train_epoch, validate, test_ensemble
 from .flow import PropagateFlow, RNVP, IAF, BayesianLinearFlow, BayesianNetworkFlow, InputSkipFlowNetwork
@@ -27,6 +26,6 @@ __all__ = [
     "network_density_reduction", "create_layer_name_list", "input_inclusion_prob",
     "expected_number_of_weights", "include_input_from_layer", "average_path_length", "prob_width",
     "get_weight_and_bias_std", "get_weight_and_bias", "relu_activation",
-    "local_explain_relu", "local_explain_relu_magnitude", "local_explain_piecewise_linear_act",
-    "train_epoch", "validate", "test_ensemble", "plotting",
+    "local_explain_relu", "local_explain_relu_magnitude", "local_explain_piecewise_linear_act", 
+    "what_if_explanations", "train_epoch", "validate", "test_ensemble", "plotting",
 ]
--- LBBNN/plotting/__init__.py
+++ LBBNN/plotting/__init__.py
@@ -1,11 +1,11 @@
 from .graphs import plot_whole_path_graph, plot_whole_path_graph_weight, run_path_graph, run_path_graph_weight, plot_path_individual_classes
-from .explanations import plot_local_contribution_empirical, plot_local_explain_piecewise_linear_act
+from .explanations import plot_local_contribution_empirical, plot_local_explain_piecewise_linear_act, plot_what_if_explanations
 from .images import plot_model_vision_image, plot_local_contribution_images_contribution_empirical, plot_local_contribution_images_contribution_empirical_magnitude
 from .metrics import get_metrics, save_metrics
 
 __all__ = [
     "plot_whole_path_graph", "plot_whole_path_graph_weight", "run_path_graph", "run_path_graph_weight", "plot_path_individual_classes",
-    "plot_local_contribution_empirical", "plot_local_explain_piecewise_linear_act",
+    "plot_local_contribution_empirical", "plot_local_explain_piecewise_linear_act", "plot_what_if_explanations",
     "plot_model_vision_image", "plot_local_contribution_images_contribution_empirical", "plot_local_contribution_images_contribution_empirical_magnitude",
     "get_metrics", "save_metrics",
 ]
