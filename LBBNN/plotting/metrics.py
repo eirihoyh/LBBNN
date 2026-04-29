bbc7d4a764aef8d7dfb81ae192306e7d41e83c8e
eirihoyh <eirik.hoy@gmail.com>
2026-04-29 10:17:11 +0200

    save metrics function now uses get metrics function

--- LBBNN/plotting/metrics.py
+++ LBBNN/plotting/metrics.py
@@ -66,45 +66,29 @@ def save_metrics(
     Returns:
         A tuple containing the saved median-metric path and full-metric path.
     """
-    clean_alpha_list = insp.clean_alpha(net, threshold)
-    p = clean_alpha_list[0].shape[1]
-
-    layer_names = insp.create_layer_name_list(
-        n_layers=len(clean_alpha_list) + 1,
-    )
-    density, used_weights, total_weights = insp.network_density_reduction(
-        clean_alpha_list
-    )
-    mean_path_length, _ = insp.average_path_length(clean_alpha_list)
-    include_inputs = insp.include_input_from_layer(clean_alpha_list)
-
-    metrics_median = {
-        "layer_names": layer_names,
-        "tot_weights": total_weights,
-        "used_weights": used_weights,
-        "density": density,
-        "avg_path_length": mean_path_length,
-        "include_inputs": include_inputs,
-    }
+    m = get_metrics(net, threshold)
 
     ensure_parent(path)
 
+    metrics_median = {
+        "layer_names": m["layer_names"],
+        "tot_weights": m["tot_weights"],
+        "used_weights": m["used_weights_median"],
+        "density": m["density_median"],
+        "avg_path_length": m["avg_path_length"],
+        "include_inputs": m["include_inputs"],
+    }
     median_path = f"{path}_median.npy"
     np.save(f"{path}_median", metrics_median)
 
-    expected_nr_weights = insp.expected_number_of_weights(net)
-    input_inclusion_prob = insp.input_inclusion_prob(net)
-    width_prob = insp.prob_width(net, p)
-
     metrics_full = {
-        "layer_names": layer_names,
-        "tot_weights": total_weights,
-        "expected_nr_of_weights": expected_nr_weights,
-        "density": expected_nr_weights / total_weights,
-        "expected_nr": input_inclusion_prob,
-        "width_prob": width_prob,
+        "layer_names": m["layer_names"],
+        "tot_weights": m["tot_weights"],
+        "expected_nr_of_weights": m["expected_nr_weights_full"],
+        "density": m["density_full"],
+        "expected_nr": m["input_inclusion_prob"],
+        "width_prob": m["width_prob"],
     }
-
     full_path = f"{path}_full.npy"
     np.save(f"{path}_full", metrics_full)
 
